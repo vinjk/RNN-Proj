c461563baf911e128576cfd67bfd062458735f83
Vineet Jacob Kuruvilla <vineet.jk@gmail.com>
2017-11-04 23:03:05 +0800

    Completed project

--- my_answers.py
+++ my_answers.py
@@ -3,7 +3,11 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.optimizers import RMSprop
+
 import keras
+
+from collections import Counter
 
 
 # TODO: fill out the function below that transforms the input series 
@@ -12,6 +16,10 @@ def window_transform_series(series, window_size):
     # containers for input/output pairs
     X = []
     y = []
+    
+    for idx in range(len(series)-window_size):
+        X.append(series[idx:idx+window_size])
+        y.append(series[idx+window_size])
 
     # reshape each 
     X = np.asarray(X)
@@ -23,12 +31,28 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape = (window_size,1)))
+    model.add(Dense(1))
+    
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
+    text = [x.lower() for x in text]
+    text = ''.join(text)
+    counts = Counter(text)
+    
+    for l in counts.keys():
+        if l in punctuation:
+            pass
+        elif ord(l) >= 97 and ord(l) <=122:
+            pass
+        else:
+            text = text.replace(l,' ')
+            
 
     return text
 
@@ -37,10 +61,22 @@ def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
+    
+    for idx in range(0, len(text)-window_size, step_size):
+        #print(idx)
+        inputs.append(text[idx:idx+window_size])
+        outputs.append(text[idx+window_size])
 
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200, input_shape = (window_size, num_chars)))
+    model.add(Dense(num_chars, activation = 'softmax'))
+    
+    #RMSprop(lr=0.005)
+    #model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    
+    return model
